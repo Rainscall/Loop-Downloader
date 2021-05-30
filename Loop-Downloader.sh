34c453c68ce056374fb90f720ee67bac652c996e
Ashes26 <50625736+Ashes26@users.noreply.github.com>
2021-05-30 17:32:08 +0800

    Create Loop-Downloader.sh

---
+++ Loop-Downloader.sh
@@ -0,0 +1,17 @@
+clear
+rm -f testfile.test
+rm -f testfile.test.*
+clear
+echo start test
+echo -e press  "\033[41;37m Ctrl+C \033[0m"  to exit
+echo -n "Enter your test URL(with http://):"
+read  test_URL   
+finished_times=0
+for i in {1..100}
+do
+finished_times=$(($finished_times+1))
+wget $test_URL -q --show-progress -O testfile.test
+rm testfile.test
+echo -e  Test "\033[41;37m $finished_times \033[0m" completed
+done
+exit 0
