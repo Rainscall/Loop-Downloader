8c9ba6fc35158abff3b928ee9e7879ecbd8bfbe9
Ashes26 <50625736+Ashes26@users.noreply.github.com>
2021-06-07 22:58:31 +0800

    Create Loop-downloader-Android-Mix2S-MIUI-juiceSSH.sh

---
+++ Loop-downloader-Android-Mix2S-MIUI-juiceSSH.sh
@@ -0,0 +1,17 @@
+clear
+rm -f testfile.test
+rm -f testfile.test.*
+clear
+echo start test
+echo -e press  "\033[41;37m Ctrl+C \033[0m"  to exit
+echo -n "Enter your test URL(with http://):"
+read  test_URL
+finished_times=0
+while :
+do
+finished_times=$(($finished_times+1))
+curl -s $test_URL > testfile.test
+rm testfile.test
+echo -e  Test "\033[41;37m $finished_times \033[0m" completed
+done
+exit 0
