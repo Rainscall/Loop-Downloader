cbc685cc7717dc5dd3cddd10feba59177b5c75b7
Ashes26 <50625736+Ashes26@users.noreply.github.com>
2021-06-07 19:53:46 +0800

    现已支持Windows10&Windows Server 2016或更高版本

---
+++ Loop-Downloader-Windows.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+cls
+echo This script only supports Windows 10 or Windows Server 2016 and above
+echo Start test
+echo Press Ctrl+C to exit
+set /a finished_times=0
+set /p URL=Enter your test URL:
+:Loop
+echo Downloading....
+curl --output testfile.test  -s %URL%
+del testfile.test
+set /a finished_times=finished_times+1
+echo Test !finished_times! completed
+goto Loop
+pause
