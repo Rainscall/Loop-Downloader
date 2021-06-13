5665d5119b60857320801cee2f4489a6520cd6a6
Ashes26 <50625736+Ashes26@users.noreply.github.com>
2021-06-13 16:18:59 +0800

    Support IPV6

--- Loop-Downloader.sh
+++ Loop-Downloader.sh
@@ -2,11 +2,23 @@ clear
 rm -f testfile.test
 rm -f testfile.test.*
 clear
+curl ipv6.ip.sb -s
+if [ $? -eq 7 ]; then
+clear
+echo -e Your server "\033[41;37m NOT \033[0m" support IPV6!!! 
+else
+clear
+echo -e Your server support IPV6
+fi
+IPV6=0
 echo start test
 echo -e press  "\033[41;37m Ctrl+C \033[0m"  to exit
 echo -n "Enter your test URL(with http://):"
-read  test_URL   
+read  test_URL  
+echo -n "Forced use of IPV6?(0 for no, 1 for yes):"
+read  IPV6
 finished_times=0
+if [ $IPV6 -eq 0 ]; then
 while :
 do
 finished_times=$(($finished_times+1))
@@ -14,4 +26,13 @@ wget $test_URL -q --show-progress -O testfile.test
 rm testfile.test
 echo -e  Test "\033[41;37m $finished_times \033[0m" completed
 done
+elif [ $IPV6 -eq 1 ]; then
+while :
+do
+finished_times=$(($finished_times+1))
+wget --inet6-only $test_URL -q --show-progress -O testfile.test
+rm testfile.test
+echo -e  Test "\033[41;37m $finished_times \033[0m" completed
+done
+fi
 exit 0
